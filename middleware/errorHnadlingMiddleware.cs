6977103336ca001c8c972e2f397e075c7f4833df
Akansha Jaiswal <158803162+Aka-Akansha07@users.noreply.github.com>
2025-08-05 21:29:50 +0530

    ErrorHnadlingMiddleware.cs

---
+++ middleware/errorHnadlingMiddleware.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using System.Text.Json;
+
+namespace UserManagementAPI.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+                var error = new { error = "Internal server error." };
+                await context.Response.WriteAsync(JsonSerializer.Serialize(error));
+            }
+        }
+    }
+}
