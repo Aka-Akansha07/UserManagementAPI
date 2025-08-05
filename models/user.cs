daff5aa62dd09c3ba962fd5e8e502001126c45de
Akansha Jaiswal <158803162+Aka-Akansha07@users.noreply.github.com>
2025-08-05 21:50:24 +0530

    user.cs

--- models/user.cs
+++ models/user.cs
@@ -2,17 +2,10 @@ namespace UserManagementAPI.Models
 {
     public class User
     {
-        public int Id { get; set; }
-
-        public string FirstName { get; set; } = string.Empty;
-
-        public string LastName { get; set; } = string.Empty;
-
-        public string Email { get; set; } = string.Empty;
-
-        public string Role { get; set; } = string.Empty;
-
-         public string Name => $"{FirstName} {LastName}";
+        public int Id { get; set; }         // Unique user ID
+        public string Name { get; set; }     // User's full name
+        public string Email { get; set; }    // Email address
+        public string Role { get; set; }     // Role (Admin, User, etc.)
     }
 }
 
